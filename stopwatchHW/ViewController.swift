5f30ce1a49737c488dcc62fcd518946d9343ccac
Singmeinlied <98156702+Singmeinlied@users.noreply.github.com>
2022-06-18 17:06:00 +0600

    buttons logic added

--- stopwatchHW/ViewController.swift
+++ stopwatchHW/ViewController.swift
@@ -15,19 +15,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondLabel: UILabel!
     
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
     var hours: String = "00"
     var minutes: String = "00"
     var seconds: String = "00"
     
-    var counter = 0
+    var counterSeconds = 0
     
     var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        playButton.isEnabled = false
+//        playButton.isEnabled = false
         
         hourLabel.text = hours
         minuteLabel.text = minutes
@@ -40,31 +42,52 @@ class ViewController: UIViewController {
         timer.invalidate()
         
         playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        stopButton.isEnabled = false
+        
+        reset()
     }
     
     //MARK: Pause Button Action
     @IBAction func pauseButtonTapped(_ sender: UIButton) {
         print("Pause")
         timer.invalidate()
-        playButton.isEnabled = false
+        
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
     }
     
     //MARK: Play Button Action
     @IBAction func playButtonTapped(_ sender: UIButton) {
         print("Play")
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(makeStep), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.006, target: self, selector: #selector(makeStep), userInfo: nil, repeats: true)
+        
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        stopButton.isEnabled = true
     }
     
     @objc func makeStep(){
-        print("Seconds: \(counter)")
-        if counter < 10{
-            seconds = "0\(counter)"
+        print("Seconds: \(counterSeconds)")
+        if counterSeconds < 10{
+            seconds = "0\(counterSeconds)"
         } else{
-            seconds = "\(counter)"
+            seconds = "\(counterSeconds)"
         }
         
-        counter += 1
+        counterSeconds += 1
         secondLabel.text = seconds
     }
+    func reset(){
+        hours = "00"
+        minutes = "00"
+        seconds = "00"
+        counterSeconds = 0
+        
+        hourLabel.text = hours
+        minuteLabel.text = minutes
+        secondLabel.text = seconds
+    }
+    
 }
 
